28ec15d23ecdd496d607f4e1366654b1114ca0f2
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2022-10-25 23:50:21 +0530

    Update rearrange.js

--- rearrange.js
+++ rearrange.js
@@ -1,29 +1,30 @@
 
-	const  rearrayange =(array, n)=> {
+	const  rearrange =(array, n)=> {
 	
-		let maxIndex = n - 1, minIndex = 0;
+		let maxIndex = n - 1;
+		let minIndex = 0;
 
 	
-		let maxEment = array[n - 1] + 1;
+		let maxElement = array[n - 1] + 1;
 
 		
 		for (let index = 0; index < n; index++) {
 			
 			if (index % 2 == 0) {
-				array[index] += (array[maxIndex] % maxEment) * maxEment;
+				array[index] += (array[maxIndex] % maxElement) * maxElement;
 				maxIndex--;
 			}
 
 			
 			else {
-				array[index] += (array[minIndex] % maxEment) * maxEment;
+				array[index] += (array[minIndex] % maxElement) * maxElement;
 				minIndex++;
 			}
 		}
 
 		
 		for (let index = 0; index < n; index++)
-			array[index] = Math.floor(array[index] / maxEment);
+			array[index] = Math.floor(array[index] / maxElement);
 
 		return array;
 	}
@@ -34,7 +35,7 @@
 	let n = array.length;
 
 
-	console.log(rearrayange(array, n));
+	console.log(rearrange(array, n));
 
 
 
