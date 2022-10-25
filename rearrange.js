82bf7133228220a1aa56f6ebcc563640c0c8656b
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2022-10-25 23:56:43 +0530

    Update rearrange.js

--- rearrange.js
+++ rearrange.js
@@ -1,5 +1,9 @@
-
-	const  rearrange =(array, n)=> {
+/*
+						Time Complexity = O(n)
+						Space Complexity = O(1)
+						Possible Test Cases = [2,4,6,8,10],[1,2,3,4,5,6,7,8,9],[11,12,13,14,15,16,17]  ..N numbers of sorted array.
+*/
+const  rearrange =(array, n)=> {
 	
 		let maxIndex = n - 1;
 		let minIndex = 0;
