8a0cd5a5e180e9bc8f17dcd5a1243d96b329ae23
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2022-10-25 23:46:51 +0530

    Update rearrange.js

--- rearrange.js
+++ rearrange.js
@@ -1,15 +1,40 @@
-const array =  [2, 4, 6, 8, 10];
-var left = 0;
-var right = array.length-1;
-while(left <= right){
 
-    if(left < right){
-        console.log(str(array[right])+ " " +str(array[left]),end=" ");
-    }
-    else{
-        console.log(array[left])
-    }
-    
-    left+=1;
-    right-=1;
-}
+	const  rearrayange =(array, n)=> {
+	
+		let maxIndex = n - 1, minIndex = 0;
+
+	
+		let maxEment = array[n - 1] + 1;
+
+		
+		for (let index = 0; index < n; index++) {
+			
+			if (index % 2 == 0) {
+				array[index] += (array[maxIndex] % maxEment) * maxEment;
+				maxIndex--;
+			}
+
+			
+			else {
+				array[index] += (array[minIndex] % maxEment) * maxEment;
+				minIndex++;
+			}
+		}
+
+		
+		for (let index = 0; index < n; index++)
+			array[index] = Math.floor(array[index] / maxEment);
+
+		return array;
+	}
+
+
+
+	let array = [2,4,6,8,10];
+	let n = array.length;
+
+
+	console.log(rearrayange(array, n));
+
+
+
